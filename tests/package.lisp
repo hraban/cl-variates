c1e9fd0b04f15fde4aa02c077eaa32209d25a743
Hraban Luyat <hraban@0brg.net>
2023-10-02 21:02:02 -0400

    test: shadow duplicate symbol from lift

--- tests/package.lisp
+++ tests/package.lisp
@@ -1,4 +1,5 @@
 (in-package #:common-lisp-user)
 
 (defpackage #:cl-variates-test
-  (:use #:common-lisp #:lift #:cl-variates))
+  (:use #:common-lisp #:lift #:cl-variates)
+  (:shadowing-import-from #:cl-variates #:random-element))
